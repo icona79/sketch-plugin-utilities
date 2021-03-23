d009253d3da189a999ded1248c6b7d1bbea8f0b7
Matteo Gratton <icona79@gmail.com>
2021-03-23 11:16:51 +0000

    error in the index file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import setResizingConstraint from "./layout-utils.js";
 import setSmartLayout from "./layout-utils.js";
+import textStyles from "./styles.js";
 
 export { setResizingConstraint, setSmartLayout, textStyles };
